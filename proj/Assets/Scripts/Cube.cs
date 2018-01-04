baec714e91626de8f6cc2e25413fcefb97c62240
Gabriel Anciano <gab.anciano@gmail.com>
2018-01-05 02:01:53 +0800

    First Commit
    Initial commit!

---
+++ proj/Assets/Scripts/Cube.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour {
+
+    public CubeProperties redCube;
+    public CubeProperties blueCube;
+
+    CubeProperties cubeProp;
+
+    int activeCubeProp;
+
+    private void Start()
+    {
+        activeCubeProp = 1;
+        ChangeCubeProperties();
+    }
+
+    void Update () {
+        GetComponent<Renderer>().material = cubeProp.cubeMaterial;
+        transform.Rotate(0, cubeProp.cubeSpeed, 0);
+	}
+
+    public void ChangeCubeProperties()
+    {
+        if (activeCubeProp == 1)
+        {
+            activeCubeProp = 2;
+            cubeProp = redCube;
+        }
+        else if(activeCubeProp == 2)
+        {
+            activeCubeProp = 1;
+            cubeProp = blueCube;
+        }
+    }
+}
